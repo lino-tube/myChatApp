e92a186fbe59c8bd09286790a249b20fe91a9223
lino-tube <ST10533499@rcconnect.edu.za>
2026-05-23 11:17:57 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/mychatapp/Login.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mychatapp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//this is where all my programs will be stored
+
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+    
+    //this is where the user will store their user name
+    String username;
+    
+    //this is where the user will store their password
+    String password;
+    
+    //this is where the user will store their number
+    String phoneNumber;
+    
+    //check whether the username is correct or not
+    public boolean checkUserName(String username){
+    //username must contain an underscore(_)
+    //username must not be longer than 5 characters
+    
+    //the username is now being returned
+    return username.contains("_") && username.length() <=5;
+    }
+    
+    //checks whether the password is valid or invalid
+    public boolean checkPasswordComplexity(String password){
+    //password must at least be 8 characters long
+    //password must contain at least 1 capital letter
+    //paasword must have at least 1 number
+    //password must have at least 1 speacial letter
+    boolean hasCapital = false;
+    boolean hasNumber = false;
+    boolean hasSpecial = false;
+    
+    for (int i = 0; i < password.length(); i++) {
+    char c = password.charAt(i);
+    //checks whether the password contains a capital letter
+    if (Character.isUpperCase(c)){
+       hasCapital = true;
+    }
+    //checks whether the password contains a number
+    else if (Character.isDigit(c)){
+       hasNumber = true;
+    }
+    //checks whether the password contains a special character
+    else if (Character.isLetterOrDigit(c)){
+       hasSpecial = true;
+    }
+   }
+   return password.length() >=8 && hasCapital && hasNumber && hasSpecial;
+  }
+ 
+   //checks whether the number is entered correctly or not
+   public boolean checkCellPhoneNumber(String phoneNumber) {
+       /*
+        *the number must have a South African international code(+27)
+        *that follows with 9 digits after
+        */
+       return phoneNumber.startsWith("+27") && phoneNumber.length() <=12;
+   } 
+   
+   //checks whether the user details have been successfully registered
+   public String registerUser(String username, String password, String phoneNumber) {
+   
+       /*
+        *Confirms whether the user detials are correct,
+        *stores the user detials if everything is correct,
+        *and returns specific messages to the user
+        */
+       if(!checkUserName(username)) {
+         return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+       }
+       
+       if(!checkPasswordComplexity(password)) {
+         return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+       }
+       
+       if(!checkCellPhoneNumber(phoneNumber)) {
+         return "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.";
+       }
+       
+       this.username = username;
+       this.password = password;
+       this.phoneNumber = phoneNumber;
+       
+       return "User registered successfully.";
+   }
+   //allows the user to login using the same details as the registration details
+   public boolean loginUser(String username, String password){
+     return this.username.equals(username) && this.password.equals(password);
+   }
+   
+   //returns whether the usename and password are correct
+   public String returnLoginStatus(boolean success) {
+     if(success) {
+       return "Welcome " + username + " it is great to see you again.";
+     }
+     else {
+       return "Username or password is incorrect, please try again";
+     }
+   }
+}    
---
+++ src/main/java/com/mycompany/mychatapp/Main.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mychatapp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//this is where all my programs will be stored
+
+
+//imports the Scanner class so we can take input from the user
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+
+//the programm will now be interacting with user
+public class Main {
+    public static void main(String[] args) {
+    
+        //the scanner will allow the user to enter their details
+        Scanner input = new Scanner(System.in);
+        
+        /*
+         an object of the login class is being created, 
+         so we can call its methods
+        */
+        Login login = new Login();
+        
+        //user registration process for the chat app
+        System.out.println("=== USER REGISTRATION ===");
+        
+        System.out.println("Enter a username: ");
+        String username = input.nextLine();
+        
+        System.out.println("Enter a password: ");
+        String password = input.nextLine();
+        
+        System.out.println("Enter your South African phone number (+27...): ");
+        String phoneNumber = input.nextLine();
+        
+        //registerUser method gets called and the message it returns gets stored
+        String response = login.registerUser(username, password, phoneNumber);
+        
+        //registration message is being shown
+        System.out.println(response);
+        
+        //user login process for the chat app 
+        System.out.println("\n=== USER LOGIN ===");
+        
+        System.out.println("Enter your username: ");
+        String loginUsername = input.nextLine();
+        
+        System.out.println("Enter your password: ");
+        String loginPassword = input.nextLine();
+        
+        //loginUser method gets called to check if the details entered matches the stored ones
+        boolean loggedIn = login.loginUser(loginUsername, loginPassword);
+        
+        if(loggedIn) {
+            //if the user has logged in successfully, we welcome them to the chatapp
+            System.out.println("=====================");
+            System.out.println("WELCOME TO QUICKCHAT.");
+            System.out.println("=====================");
+            
+            boolean running = true;
+            
+            /*
+             an object of the message class is being created, 
+             so we can call its methods
+            */
+            
+            while(running) {
+                //display the menu choices to the user
+                System.out.println("1) Send Messages");
+                System.out.println("2) Show recently sent messages");
+                System.out.println("3) Quit");
+                
+                //We are prompting the user to enter their option(1, 2 or 3)
+                System.out.println("Choose an option: ");
+                //reads the user input 
+                int choice = input.nextInt();
+                input.nextLine();
+                
+                switch(choice) {
+                    case 1:
+                        //we asking the user to enter how mny messages they wish to send.
+                        System.out.println("How many messages would you like to send?");
+                        int numMessages = input.nextInt();
+                        input.nextLine();
+                        
+                        ////loops according to the number of messages the user will be sending
+                        for(int i = 0; i < numMessages; i++){
+                            int messageNumber = i + 1;
+                            Message msg = new Message(messageNumber);
+                        
+                            System.out.println("--- Message " + messageNumber + " ---");
+                            
+                            //we are prompting the user to enter the number of the person or people they sending messages to
+                            System.out.println("Enter recipient cell phone number: ");
+                            String recipientCell = input.nextLine();
+                            
+                            //checkRecipientCell method gets called and the message it returns gets stored
+                            String feedback = msg.checkRecipientCell();
+                            System.out.println(feedback);
+                            
+                            //we are prompting the user to type the message they want to send
+                            System.out.println("Enter your message: ");
+                            String messageText = input.nextLine();
+                            
+                            //we are checking if the message length is correct or not and displaying the response messages.
+                            System.out.println(msg.checkMessageLength(messageNumber, recipientCell, messageText));
+                            if (messageText.length() > 250){
+                                int over = messageText.length() - 250;
+                                System.out.println("Message exceeds 250 characters by X" + over + "; please reduce the size.");
+                            }else{
+                                System.out.println("Message ready to send.");
+                            }
+                            
+                        }
+                        break;
+                     case 2: 
+                        System.out.println("Comming Soon.");
+                        break;
+                    case 3:  
+                        System.out.println("Thank you for using QuickChat.");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Inavlid option, please choose either 1, 2, or 3. ");
+                }
+            }
+        }else{
+            //If the user's login failed, we tell them to try again
+            System.out.println("Login was unsuccessful, please try again.");
+        }
+    }
+}
