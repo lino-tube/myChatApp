88eb86332fa1b7596c87499bddff576fc9bed89e
lino-tube <ST10533499@rcconnect.edu.za>
2026-05-23 14:29:25 +0200

    Created a Message.java class and MessageTest.java test class

---
+++ src/main/java/com/mycompany/mychatapp/Message.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mychatapp;
+
+import java.util.Random;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class Message {
+    private String messageID;
+    private int messageNumber;
+    private String recipientCell;
+    private String messageText;
+    private String messageHash;
+    
+    
+    private static String[] sentMessages;
+    private static int messageIndex = 0;
+    private static int totalMessagesSent = 0;
+    
+    public Message(int messageNumber) {
+        this.messageNumber = messageNumber;
+        this.recipientCell = recipientCell;
+        this.messageText = messageText;
+        this.messageHash = generateMessageHash();
+        this.messageID = createMessageID();
+    }
+    
+    private String createMessageID(){
+        Random random = new Random ();
+        long id = (long) random.nextInt(1000000000) + 1000000000;
+        return String.valueOf(id);
+    }
+        
+    public boolean checkMessageID(){
+      return messageID.length() == 10;
+    }
+    
+    public String generateMessageHash(){
+        //Taking the first two characters of the message ID
+        String idPart = messageID.substring(0, 2);
+        
+        //In order to find the first and last words, we must split the message into words.
+        String[] words = messageText.split(" ");
+        String firstWord = words[0];
+        String lastWord = words[words.length - 1];
+        
+        //combine the idPart, messageNumber, firstWord and lastWord together, to form a message hash
+        String hash = idPart + ":" + messageNumber + ":" + firstWord + lastWord;
+        //return everything in capital letters
+        return hash.toUpperCase();
+    }
+    
+     public String checkRecipientCell(){
+        
+        if(recipientCell.startsWith("+27") && recipientCell.length() <=12){
+            return"Cell phone number successfully captured.";
+        }else{
+            return"Cell phone number is incorrectly formatted or does not contain an international code. Please correct the number and try again.";
+        }
+    }
+    
+    public String sentMesssage() {
+        System.out.println("What would yoou like to do with your message?");
+        System.out.println("1) Send Message");
+        System.out.println("2) Disregard Message");
+        System.out.println("3) Store Message to send later");
+        
+        int option = 0;
+        
+        switch(option) {
+            case 1:
+                sentMessages[messageIndex] = printMessage();
+                messageIndex++;
+                totalMessagesSent++;
+                return"Message successfully sent";
+            case 2:
+                return"Press 0 to delete the message";
+            case 3:
+                storeMessage(messageID, recipientCell, messageText);
+                return"Message successfully stored";
+            default:
+                return"Invalid option. Please choose option 1, 2, or 3";
+        }
+    }
+    //we are checking if the message length is correct or not and returning the response messages.
+    public String checkMessageLength(int messageNumber, String recipientCell, String messageText){
+        if (messageText.length() > 250){
+            int over = messageText.length() - 250;
+            return "Message exceeds 250 characters by X" + over + "; please reduce the size.";
+        }else{
+            return"Message ready to send.";
+        }
+    }
+     
+    public String printMessage() {
+        return "Message ID: " + messageID + "\n" +
+                "Message Hash: " + messageHash + "\n" +
+                "Recipient: " + recipientCell + "\n" +
+                "Message: " + messageText;
+    }
+    
+    public int returnTotalMessages() {
+        return totalMessagesSent;
+    }
+    
+    public void storeMessage(String messageID, String recipientCell, String messageText) {  
+        JSONObject obj = new JSONObject();
+            obj.put("messageID", this.messageID); 
+            obj.put("recipientCell", this.recipientCell); 
+            obj.put("messageText",   this.messageText); 
+
+        try (java.io.FileWriter fw = new java.io.FileWriter("messages.json", true)) {
+            fw.write(obj.toString());
+        } catch (IOException e){
+            System.out.println("Error storing message: " + e.getMessage());
+        }
+   } 
+
+}
+
--- src/test/java/com/mycompany/mychatapp/LoginTest.java
+++ src/test/java/com/mycompany/mychatapp/LoginTest.java
@@ -4,10 +4,159 @@
  */
 package com.mycompany.mychatapp;
 
+//this import allows us to test our methods using @Test
+import org.junit.Test;
+
+//allows us to use all our assertions
+import static org.junit.Assert.*;
+
 /**
  *
- * @author Linothando
+ * @author Student
  */
 public class LoginTest {
     
+    //testing the username
+    @Test
+    public void testUsernameInvalidation() {
+        /*
+         *Entering an incorret username "kyle!!!!!!!"
+         *To check if the registerUser() will return the correct error message
+         */
+        
+        //We are creating a login object to test its method
+        Login login = new Login();
+        String result = login.registerUser("kyle!!!!!!!", "Ch&&sec@ke99!", "+27838968976");
+        
+        //Checking if the actual result is the exact same as the expected result
+        assertEquals(
+           "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.",
+            result    
+        );
+    }
+    
+    //Testing if the username is true
+    @Test
+    public void testUsernameValidationBoolean(){
+        //Username("kyl_1") is true, it conatains an uderscore and is no more than five characters long.
+        Login login = new Login();
+        assertTrue(login.checkUserName("kyl_1"));
+    
+    }
+    
+    //Testing if the username is false
+    @Test
+    public void testUsernameInvalidationBoolean(){
+        //Username("kyle!!!!!!!") is invalid, which makes it false.
+        Login login = new Login();
+        assertFalse(login.checkUserName("kyle!!!!!!!"));
+    }
+    
+    //Testing the password
+    @Test
+    public void testPasswordInvalidation() {
+         /*
+         *Entering an incorret password "password"
+         *To check if the registerUser() will return the correct error message
+         */
+         Login login = new Login();
+         String result = login.registerUser("kyl_1", "password", "+27838968976");
+         
+         assertEquals(
+                 "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.",
+                 result
+         );
+    
+    }
+    
+    //Testing if the password is true
+    @Test
+    public void testPasswordValidationBoolean() {
+        //Password("Ch&&sec@ke99!") is true, it contains a capital letter, a number, a special character and is at least eight characters in long.
+        Login login = new Login();
+        assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+    }
+    
+    //Testing if the password is false
+    @Test
+    public void testPasswordInvalidationBoolean() {
+        //Password("password") is false, it does not meet the complexity rules
+        Login login = new Login();
+        assertFalse(login.checkPasswordComplexity("password"));
+    }
+    
+    //Testing the cell phone number
+    @Test
+    public void testPhoneNumberInvalidation() {
+        /*
+         *Entering an incorret phone number "0838968976"
+         *To check if the registerUser() will return the correct error message
+         */
+        Login login = new Login();
+        String result = login.registerUser("kyl_1", "Ch&&sec@ke99!", "0838968976");
+        
+        assertEquals(
+                "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.",
+                result
+        );
+    }
+    
+    //Testing if the phone number is true
+    @Test
+    public void testPhoneNumberValidationBoolean() {
+        //Phone Number("+27838968976") is true, it conatains a South African international code.
+        Login login = new Login();
+        assertTrue(login.checkCellPhoneNumber("+27838968976"));
+    }
+    
+    //Testing if the phone number is false
+    @Test
+    public void testPhoneNumberInvalidationBoolean() {
+        //Phone Number("0838968976") is false, it does not contain a South African international code 
+        Login login = new Login();
+        assertFalse(login.checkCellPhoneNumber("0838968976"));
+    }
+    
+    //Testing if registration will be successful
+    @Test
+    public void testRegistrationSuccess() {
+         /*
+         *All inputted details should be correct.
+         *To check if the registerUser() will return the correct success message
+         */
+         Login login = new Login();
+         String result = login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+         assertEquals(
+                 "User registered successfully.",
+                 result
+         );
+    }
+    
+    //Testing if the login process will be true
+    @Test
+    public void testLoginValidation() {
+       /*
+         *Login with the same details as the registration details.
+         *To check if the registerUser() will return the correct login success message.
+         */ 
+       Login login = new Login();
+       String registerUser = login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+       
+       assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
+    }
+    
+    //Testing if the login process will be false
+    @Test
+    public void testLoginInvalidation() {
+        /*
+         *Login with the same details as the registration details, but use the wrong password.
+         *To check if the registerUser() will return the correct login error message.
+         */
+        Login login = new Login();
+         String registerUser = login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976");
+        
+        assertFalse(login.loginUser("kyl_1", "password"));
+    }
+ 
 }
+
