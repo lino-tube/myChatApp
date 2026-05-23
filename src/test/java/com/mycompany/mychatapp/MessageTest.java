955a96ab34c9b0550b4419e98ecc442fd31c9adb
lino-tube <ST10533499@rcconnect.edu.za>
2026-05-23 14:38:19 +0200

    Updated MessageText.java

--- src/test/java/com/mycompany/mychatapp/MessageTest.java
+++ src/test/java/com/mycompany/mychatapp/MessageTest.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.mychatapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,11 +14,31 @@ import static org.junit.Assert.*;
  */
 public class MessageTest {
     
-    public MessageTest() {
+    private Message message1;
+    private Message message2;
+    
+    @Before
+    public void setUp(){
+     
     }
-
     @Test
-    public void testSomeMethod() {
+    public void testCheckMessageLength_validMessage_returnsSuccess() {
+        Message msg = new Message(1);
+        msg.setMessageText("Hi Mike, can you join us for dinner tonight");
+        String result = msg.checkMessageLength(1, "+27718693002", "Hi Mike, can you join us for dinner tonight");
+        assertEquals("Message ready to send.",result);
     }
     
+    @Test
+    public void tesChecktMessageLength_over250chars_returnsFailureWithCount() {
+        Message msg = new Message(1);
+        msg.setMessageTexT("longMessage");
+        String result = msg.checkMessageLength(1, "+27718693002", "");
+        assertEquals("Message exceeds 250 characters by X[enter number here]; please reduce the size.", result);
+    }
+    
+    @Test                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    public void testCheckMessageLength_exactlyAtLImit_returnSuccess() {
+    }
 }
+
